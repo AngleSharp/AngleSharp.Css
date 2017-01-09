5791251833fd3dde14e67daefe43cf3d0c673013
Florian Rappl <rappl.florian@gmail.com>
2017-01-09 10:31:25 +0100

    Added escape helper

---
+++ src/AngleSharp.Css.Tests/Functions/CssHelpers.cs
@@ -0,0 +1,97 @@
+﻿namespace AngleSharp.Css.Tests.Functions
+{
+    using AngleSharp.Css.Dom;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CssHelpersTests
+    {
+        [Test]
+        public void EscapeEmptyString()
+        {
+            var str = "";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("", escaped);
+        }
+
+        [Test]
+        public void EscapeSimpleIdentifier()
+        {
+            var str = "abc";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("abc", escaped);
+        }
+
+        [Test]
+        public void EscapeSingleMinus()
+        {
+            var str = "-";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("\\-", escaped);
+        }
+
+        [Test]
+        public void EscapeMinusIdentifier()
+        {
+            var str = "-bc";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("-bc", escaped);
+        }
+
+        [Test]
+        public void EscapeIntegerNumber()
+        {
+            var str = "123";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("\\31 23", escaped);
+        }
+
+        [Test]
+        public void EscapeFloatingNumber()
+        {
+            var str = "1.23";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("\\31 \\.23", escaped);
+        }
+
+        [Test]
+        public void EscapeEscapedZero()
+        {
+            var str = "\0";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("\ufffd", escaped);
+        }
+
+        [Test]
+        public void EscapeZeroNumber()
+        {
+            var str = "0";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual(@"\30 ", escaped);
+        }
+
+        [Test]
+        public void EscapeDecrementOperator()
+        {
+            var str = "--a";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual("--a", escaped);
+        }
+
+        [Test]
+        public void EscapeDifferentBrackets()
+        {
+            var str = "()[]{}";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual(@"\(\)\[\]\{\}", escaped);
+        }
+
+        [Test]
+        public void EscapeDotAndHashSymbol()
+        {
+            var str = ".foo#bar";
+            var escaped = CssHelpers.Escape(str);
+            Assert.AreEqual(@"\.foo\#bar", escaped);
+        }
+    }
+}
---
+++ src/AngleSharp.Css/Dom/CssHelpers.cs
@@ -0,0 +1,50 @@
+﻿namespace AngleSharp.Css.Dom
+{
+    using AngleSharp.Attributes;
+    using AngleSharp.Text;
+    using System;
+
+    [DomName("CSS")]
+    public static class CssHelpers
+    {
+        [DomName("escape")]
+        public static String Escape(String str)
+        {
+            if (str.Length != 1 || str[0] != Symbols.Minus)
+            {
+                var sb = StringBuilderPool.Obtain();
+
+                for (var i = 0; i < str.Length; i++)
+                {
+                    var ch = str[i];
+
+                    if (ch == Symbols.Null)
+                    {
+                        ch = Symbols.Replacement;
+                    }
+                    else if (ch == 0x7f ||
+                            (ch.IsInRange(0x01, 0x1f)) ||
+                            (ch.IsDigit() && (i == 0 || (i == 1 && str[0] == Symbols.Minus))))
+                    {
+                        sb.Append(Symbols.ReverseSolidus);
+                        sb.Append(ch.ToHex());
+                        sb.Append(Symbols.Space);
+                        continue;
+                    }
+                    else if (ch < 0x80 && 
+                            !ch.IsOneOf(Symbols.Minus,Symbols.Underscore) && 
+                            !ch.IsAlphanumericAscii())
+                    {
+                        sb.Append(Symbols.ReverseSolidus);
+                    }
+
+                    sb.Append(ch);
+                }
+
+                return sb.ToPool();
+            }
+
+            return @"\-";
+        }
+    }
+}
