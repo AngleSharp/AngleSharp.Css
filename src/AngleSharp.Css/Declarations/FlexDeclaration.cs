cba9515fc2a46a98ccbfe3f2db5277700391d25a
Florian Rappl <rappl.florian@gmail.com>
2018-12-27 19:38:56 +0100

    Implementation for flex shorthand properties

--- src/AngleSharp.Css/Declarations/FlexDeclaration.cs
+++ src/AngleSharp.Css/Declarations/FlexDeclaration.cs
@@ -1,6 +1,11 @@
 namespace AngleSharp.Css.Declarations
 {
+    using AngleSharp.Css.Dom;
+    using AngleSharp.Css.Values;
+    using AngleSharp.Text;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using static ValueConverters;
 
     static class FlexDeclaration
@@ -14,8 +19,52 @@ namespace AngleSharp.Css.Declarations
             PropertyNames.FlexBasis,
         };
 
-        public static IValueConverter Converter = Or(NumberConverter, AssignInitial(0));
+        public static IValueConverter Converter = new FlexAggregator();
 
         public static PropertyFlags Flags = PropertyFlags.Shorthand;
+
+        sealed class FlexAggregator : IValueAggregator, IValueConverter
+        {
+            private static readonly IValueConverter converter = Or(None, WithAny(
+                FlexGrowDeclaration.Converter,
+                FlexShrinkDeclaration.Converter,
+                FlexBasisDeclaration.Converter));
+
+            public ICssValue Convert(StringSource source)
+            {
+                return converter.Convert(source);
+            }
+
+            public ICssValue Collect(IEnumerable<ICssProperty> properties)
+            {
+                var grow = properties.Where(m => m.Name == FlexGrowDeclaration.Name).Select(m => m.RawValue).FirstOrDefault();
+                var shrink = properties.Where(m => m.Name == FlexShrinkDeclaration.Name).Select(m => m.RawValue).FirstOrDefault();
+                var basis = properties.Where(m => m.Name == FlexBasisDeclaration.Name).Select(m => m.RawValue).FirstOrDefault();
+
+                if (grow != null || shrink != null || basis != null)
+                {
+                    return new OrderedOptions(new[] { grow, shrink, basis });
+                }
+
+                return null;
+            }
+
+            public IEnumerable<ICssProperty> Distribute(ICssValue value)
+            {
+                var options = value as OrderedOptions;
+
+                if (options != null)
+                {
+                    return new[]
+                    {
+                        new CssProperty(FlexGrowDeclaration.Name, FlexGrowDeclaration.Converter, FlexGrowDeclaration.Flags, options.Options[0]),
+                        new CssProperty(FlexShrinkDeclaration.Name, FlexShrinkDeclaration.Converter, FlexShrinkDeclaration.Flags, options.Options[1]),
+                        new CssProperty(FlexBasisDeclaration.Name, FlexBasisDeclaration.Converter, FlexBasisDeclaration.Flags, options.Options[2]),
+                    };
+                }
+
+                return null;
+            }
+        }
     }
 }
--- src/AngleSharp.Css/Declarations/FlexFlowDeclaration.cs
+++ src/AngleSharp.Css/Declarations/FlexFlowDeclaration.cs
@@ -1,6 +1,11 @@
 namespace AngleSharp.Css.Declarations
 {
+    using AngleSharp.Css.Dom;
+    using AngleSharp.Css.Values;
+    using AngleSharp.Text;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using static ValueConverters;
 
     static class FlexFlowDeclaration
@@ -13,8 +18,49 @@ namespace AngleSharp.Css.Declarations
             PropertyNames.FlexWrap,
         };
 
-        public static IValueConverter Converter = Or(NumberConverter, AssignInitial(0));
+        public static IValueConverter Converter = new FlexFlowAggregator();
 
         public static PropertyFlags Flags = PropertyFlags.Shorthand;
+
+        sealed class FlexFlowAggregator : IValueAggregator, IValueConverter
+        {
+            private static readonly IValueConverter converter = WithAny(
+                FlexDirectionConverter,
+                FlexWrapConverter);
+
+            public ICssValue Convert(StringSource source)
+            {
+                return converter.Convert(source);
+            }
+
+            public ICssValue Collect(IEnumerable<ICssProperty> properties)
+            {
+                var direction = properties.Where(m => m.Name == FlexDirectionDeclaration.Name).Select(m => m.RawValue).FirstOrDefault();
+                var wrap = properties.Where(m => m.Name == FlexWrapDeclaration.Name).Select(m => m.RawValue).FirstOrDefault();
+
+                if (direction != null || wrap != null)
+                {
+                    return new OrderedOptions(new[] { direction, wrap });
+                }
+
+                return null;
+            }
+
+            public IEnumerable<ICssProperty> Distribute(ICssValue value)
+            {
+                var options = value as OrderedOptions;
+
+                if (options != null)
+                {
+                    return new[]
+                    {
+                        new CssProperty(FlexDirectionDeclaration.Name, FlexDirectionDeclaration.Converter, FlexDirectionDeclaration.Flags, options.Options[0]),
+                        new CssProperty(FlexWrapDeclaration.Name, FlexWrapDeclaration.Converter, FlexWrapDeclaration.Flags, options.Options[1]),
+                    };
+                }
+
+                return null;
+            }
+        }
     }
 }
